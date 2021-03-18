0ed2a8299827c41415fd91459f7e3147be34bd13
Michael Kramps <michaelkramps@carfax.com>
2021-03-18 10:46:20 -0500

    remove unused import

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {waitFor, render, screen, fireEvent} from "@testing-library/react";
+import {render, screen, fireEvent} from "@testing-library/react";
 
 import Calculator from "../answers/Calculator/Calculator";
 
